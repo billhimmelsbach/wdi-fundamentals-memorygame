12798d72d41bbfc36ecdd348ad94c9b660477a1f
billhimmelsbach <whimmels@gmail.com>
2016-06-25 09:39:16 -0700

    using JS added a for loop to create card divs

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,18 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo===cardFour){
-  alert('You found a match!')
-}
-else {
-  alert('Sorry, try again')
+//if (cardTwo===cardFour){
+  //alert('You found a match!')
+//}
+//else {
+  //alert('Sorry, try again.')
+//}
+
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (i=0; i < card.Length; i++) {
+    var cardAdd = document.createElement('div');
+    cardAdd.className = 'card';
+    board.appendChild('cardAdd');
+  }
 }
